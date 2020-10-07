610e80669a75ea43a1e5f2ebf2ce96c3c59d19c1
Nicolas Molina Monroy <hi@nicobytes.com>
2020-10-06 20:33:20 -0400

    :tada: init commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../styles/components/App.styl';
 
-const App = () => <h1>Hello React!</h1>;
+const App = () => {
+
+  useEffect(() => {
+    loadYoutubeApi();
+  }, []);
+
+  const loadYoutubeApi = () => {
+    const script = document.createElement("script");
+    script.src = "https://apis.google.com/js/api.js";
+
+    script.onload = () => {
+      gapi.load('client', () => {
+        gapi.client.setApiKey('');
+        gapi.client.load('youtube', 'v3', () => {
+          console.log('load');
+        });
+      });
+    };
+
+    document.body.appendChild(script);
+  }
+
+  return (
+    <div>
+      <h1>Hola</h1>
+    </div>
+  )
+};
 
 export default App;
